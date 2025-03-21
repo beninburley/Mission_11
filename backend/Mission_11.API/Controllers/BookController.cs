b3e088101c0b1cb8a2c083e7e170a56f16601661
beninburley <130599399+beninburley@users.noreply.github.com>
2025-03-21 12:34:13 -0600

    File Submission

---
+++ backend/Mission_11.API/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission_11.API.Data;
+
+namespace Mission_11.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        private BookDbContext _context;
+        public BookController(BookDbContext temp)
+        {
+            _context = temp;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks(int pageHowMany = 5, int pageNum = 1, string sortOrder = "asc")
+        {
+            var totalNumBooks = _context.Books.Count();
+
+            var booksQuery = _context.Books.AsQueryable();
+
+            // Sorting logic
+            booksQuery = sortOrder.ToLower() == "desc"
+                ? booksQuery.OrderByDescending(b => b.Title)
+                : booksQuery.OrderBy(b => b.Title);
+
+            var apiReturn = booksQuery
+                .Skip((pageNum - 1) * pageHowMany)
+                .Take(pageHowMany)
+                .ToList();
+
+            return Ok(new
+            {
+                Books = apiReturn,
+                TotalNumBooks = totalNumBooks
+            });
+        }
+
+    }
+}
---
+++ backend/Mission_11.API/Data/BookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission_11.API.Data
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
